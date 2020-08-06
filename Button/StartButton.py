bb1d9bef659adc69ded0b6f335267b3a4f968d71
Benedict Ee <69200986+Benedict98ee@users.noreply.github.com>
2020-08-06 12:53:23 +0800

    created StartButton and editted main

---
+++ Button/StartButton.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+win = tk.Tk()
+StartButton = tk.Button(win, text="Press to Start!", bg="red", font=("Arial", 40))
+StartButton.pack()
+win.mainloop()
--- ben.py
+++ ben.py
@@ -1,1 +1,25 @@
-print("Hello Isaac")
+import tkinter as tk
+
+class Application(tk.Frame):
+    #declaring independent constructor
+    def __init__(self, master=None):
+        #getting inherited variables and properties
+        super().__init__(master);
+        self.master=master;
+
+
+
+
+
+
+def main():
+    root = tk.Tk()
+    app = Application(root);
+    root.title("Scissors, paper,stone");
+    root.geometry("480x480");
+    root.resizeable();
+    app.mainloop();
+    
+
+if __name__ == "__main__":
+    main()
